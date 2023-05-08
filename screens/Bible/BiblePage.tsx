562cfdd4c7094eb26b362742c2d6dee13cd9d6fc
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2023-05-08 02:54:01 +0100

    Calling API

--- screens/Bible/BiblePage.tsx
+++ screens/Bible/BiblePage.tsx
@@ -1,12 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text, View, TextInput, TouchableOpacity, Image, Platform, Alert, Button, KeyboardAvoidingView, Pressable, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+//bd20409d11afec8ec1d7a7dfebf3334a
 
 export default function BiblePage() {
+  const [bibleVersions, setBibleVersions] = useState([]);
+
+  const getBibleVersions = async () => {
+    try {
+      const response = await fetch('https://api.scripture.api.bible/v1/bibles', {
+        method: 'GET',
+        headers: {
+          'api-key': '',
+        },
+      });
+
+      const data = await response.json();
+      const versions = data.data.map((bible) => ({
+        id: bible.id,
+        abbreviation: bible.abbreviation,
+      }));
+
+      setBibleVersions(versions);
+      console.log(versions);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getBibleVersions();
+  }, []);
+
   return (
-    <View>
-      <Text>BiblePage</Text>
-    </View>
-  )
+    <SafeAreaView>
+      {/* Display bible versions */}
+      {bibleVersions.map((bible) => (
+        <Text key={bible.id}>{bible.abbreviation}</Text>
+      ))}
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
