1f235f3ded7e40344ace84f80e22cd171e5c79af
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2023-06-08 11:30:36 +0100

    - Added firebase and post type

--- screens/Bible/BiblePost.tsx
+++ screens/Bible/BiblePost.tsx
@@ -2,17 +2,87 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
+import { database } from '../../config/firebase';
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import RNPickerSelect from "react-native-picker-select";
 
 
 function BiblePost({route, navigation}) {
     const { selectedVerses } = route.params;
     const [userOpinion, setUserOpinion] = useState("");
+    const [postType, setPostType] = useState("");
 
-    const postOpinion = () => {
-        // Function to handle the posting of the user opinion to the database
-        console.log(userOpinion);
-    }
-    console.log(selectedVerses);
+    const postOpinion = async () => {
+        if (userOpinion === "" || postType === "") {
+            alert("Please enter an opinion and select a post type.");
+            return;
+        }
+
+        try {
+            const auth = getAuth();
+            const uid = auth.currentUser.uid;
+
+            // I want the timestamp to be in the format of "2021-10-10 10:10:10 and I want it to be in the user's timezone"
+
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+            const seconds = date.getSeconds();
+
+            const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+            const postData = {
+                userOpinion,
+                postType,
+                BibleInformation: selectedVerses.map(verse => ({
+                    BibleBook: verse.book,
+                    BibleChapter: verse.chapter,
+                    BibleVerse: verse.verse,
+                    BibleText: verse.text,
+                })),
+                timestamp,
+                uid,
+                praises: 0,
+                comments: 0,
+            };
+            
+            const collectionPath = postType === "public" ? "publicPosts" : "privatePosts";
+
+            await addDoc(collection(database, collectionPath), postData);
+
+            alert("Post successful");
+            navigation.goBack();
+
+        } catch (error) {
+            console.log(error);
+        }
+
+        // first check if the userOpinion is empty
+        // if it is empty, then show an alert(telling the user to enter an opinion)
+        // if it is not empty, then post the opinion to the database
+        // the fields that need to be posted are:
+        // 1. userOpinion
+        // 2. postType
+        // 3. selectedVerses
+        //  3a. book
+        //  3b. chapter
+        //  3c. verse
+        //  3d. text
+        // 4. timestamp
+        // 5. uid (of the user who posted the opinion so that we can retrieve the user's name and profile picture later when we display the post)
+        // 6. praises (initially set to 0)
+        // 7. comments (initially set to 0)
+
+        // if the postType is public, then post the opinion to the public collection(This is the default, so everyone can see it, you don't need to be a connection to see it)
+        // if the postType is private, then post the opinion to the private collection(This is the case when the user wants to post an opinion only their connections can see)
+        
+        // if the post is successful, then show an alert saying "Post successful" then navigate back to the home screen
+    };
     return (
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView 
@@ -22,9 +92,7 @@ function BiblePost({route, navigation}) {
                 <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Ionicons name="arrow-back" size={32} style={styles.backArrow}/>
                 </TouchableOpacity>
-                <Text style={styles.title}>Create a revelation</Text>
-            
-                
+                <Text style={styles.title}>Create a revelation</Text>                
                 {
                     selectedVerses && selectedVerses.map((verse, index) => (
                         <View key={index} style={styles.verseContainer}>
@@ -40,6 +108,19 @@ function BiblePost({route, navigation}) {
                     placeholder="Enter your opinion on the verse..."
                     multiline
                 />
+               <RNPickerSelect
+                onValueChange={(value) => setPostType(value)}
+                items={[
+                    { label: "Public", value: "public" },
+                    { label: "Private", value: "private" },
+                ]}
+                placeholder={{
+                    label: "Select a post type...",
+                    value: null,
+                }}
+                style={pickerSelectStyles}
+                value={postType}
+            />
                 <TouchableOpacity style={styles.buttonContainer} onPress={postOpinion}>
                     <Text style={styles.buttonText}>Post</Text>
                 </TouchableOpacity>
@@ -99,6 +180,30 @@ const styles = StyleSheet.create({
     backArrow: {
         marginBottom: 20,
     }
+});
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 4,
+        color: 'black',
+        paddingRight: 30 // to ensure the text is never behind the icon
+    },
+    // Similar styling for Android
+    inputAndroid: {
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 4,
+        color: 'black',
+        paddingRight: 30 // to ensure the text is never behind the icon
+    },
 });
 
 export default BiblePost;
