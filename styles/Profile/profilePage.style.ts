7b0036d69e26e7c82555cae6b305b0c24b622623
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2023-06-28 00:10:10 +0100

    All users post are showing

--- styles/Profile/profilePage.style.ts
+++ styles/Profile/profilePage.style.ts
@@ -4,10 +4,10 @@ import { StyleSheet, Dimensions } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height / 2,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#ffffff',
+    padding: 10,
   },
   avatar: {
     marginBottom: 15,
@@ -52,8 +52,68 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     color: '#666',
     paddingLeft: 20,
-  paddingTop: 20,
+    paddingTop: 20,
+  },
+  postContainer: {
+    width: '100%',
+    backgroundColor: '#ffffff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#f0f0f0',
+    padding: 15,
+  },
+  postHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  postUser: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  postTitle: {
+    fontSize: 18,
+    fontWeight: '700',
+    marginBottom: 5,
+  },
+  postUsername: {
+    marginRight: 5,
+    fontWeight: 'bold',
+  },
+  postUserImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  postBibleInformation: {
+    marginTop: 10,
+    backgroundColor: '#f5f8fa',
+    padding: 10,
+    borderRadius: 10,
+  },
+  postBibleReference: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  postBibleText: {
+    fontSize: 14,
+    marginTop: 5,
+  },
+  postUserOpinion: {
+    fontSize: 15,
+    color: '#333',
+    lineHeight: 20,
+    marginTop: 10,
+  },
+  postTimestamp: {
+    fontSize: 12,
+    color: '#888',
+    marginTop: 15,
+  },
+  scene : {
+    flex: 1,
   },
 });
 
 export default styles;
+
